4e3129d9d769ea391e19b5a4eb85fa140fcf38c2
Vladimir Golub <golubvlauri@gmail.com>
2022-11-19 11:15:03 +0200

    add getDocumentWithCover function

--- src/pdf-generator/services/pdf-generator.service.ts
+++ src/pdf-generator/services/pdf-generator.service.ts
@@ -3,6 +3,8 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import Handlebars from 'handlebars';
 import puppeteer  from 'puppeteer';
+// @ts-ignore
+import * as PDFMerger from 'pdf-merger-js';
 
 @Injectable()
 export class PdfGeneratorService {
@@ -40,31 +42,52 @@ export class PdfGeneratorService {
     return template({ text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dolorem in maiores minima quam qui quis quod repudiandae vitae! Aliquid animi commodi consectetur debitis doloribus earum, eos est fugiat libero maiores nam nesciunt perferendis placeat quis quos tempora velit voluptas, voluptate. Aliquid aspernatur deserunt esse fuga laboriosam qui quo velit!' })
   }
 
-  async getDocument() {
-    const contentDocument = this.generateDocument();
-
-    const browser = await puppeteer.launch({
-      headless: true
-    });
-    const page = await browser.newPage();
-
-    await page.goto(
-      `data:text/html;charset=utf-8,${ contentDocument }`,
+  getCoverContent() {
+    const source = readFileSync(
+      join(__dirname, '../templates/cover.hbs'),
       {
-        waitUntil: 'networkidle0'
+        encoding: 'utf8'
       }
     );
 
-    const pdf = await page.pdf({
-      format: 'A4'
-    });
+    const template = Handlebars.compile(source);
 
-    await browser.close();
-
-    return pdf;
+    return template({ title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi' })
   }
 
-  getDocumentWithCover() {
+  async getDocument() {
+    const contentDocument = this.getDocumentContent();
 
+    return await this.generatePDF(
+      contentDocument,
+      {
+        format: 'A4'
+      }
+    )
+  }
+
+  async getDocumentWithCover() {
+    const coverContent = this.getCoverContent();
+    const documentContent = this.getDocumentContent();
+
+    const coverPDF = await this.generatePDF(
+      coverContent,
+      {
+        format: 'A4'
+      }
+    );
+
+    const documentPDF = await this.generatePDF(
+      documentContent,
+      {
+        format: 'A4'
+      }
+    );
+
+    const merger = new PDFMerger();
+    await merger.add(coverPDF);
+    await merger.add(documentPDF);
+
+    return await merger.saveAsBuffer();
   }
 }
